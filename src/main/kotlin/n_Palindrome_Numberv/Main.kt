2beebbeab29e85d1256a1a907dc82cc5bc1fb085
Misterxof <mishail.lukushevich@gmail.com>
2023-01-31 01:35:09 +0300

    9. Palindrome Number
    Without Int to String convertion.

--- src/main/kotlin/n_Palindrome_Numberv/Main.kt
+++ src/main/kotlin/n_Palindrome_Numberv/Main.kt
@@ -3,6 +3,7 @@ package n_Palindrome_Numberv
 fun main(args: Array<String>) {
     println(Solution().isPalindrome(123))
     println(Solution().isPalindrome(121))
+    println(Solution().isPalindrome(-121))
     println(Solution().isPalindrome(-123))
     println(Solution().isPalindrome(10))
 }
--- src/main/kotlin/n_Palindrome_Numberv/Solution.kt
+++ src/main/kotlin/n_Palindrome_Numberv/Solution.kt
@@ -1,7 +1,26 @@
 package n_Palindrome_Numberv
 
 class Solution {
+    // R = 64,69% M=73,29%
+    // R = 98,41% M=93,24% 2 min dif
     fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+
+        var xR =0
+        var xt = x
+        while (xt != 0) {
+            xR = xR * 10 + xt % 10;
+            xt /= 10;
+        }
+
+
+        val str = x.toString()
+        println("$x $xR")
+        return x == xR
+    }
+
+    // R = 62,44% M=54,34%
+    fun isPalindrome2(x: Int): Boolean {
         val str = x.toString()
         return str == str.reversed()
     }
