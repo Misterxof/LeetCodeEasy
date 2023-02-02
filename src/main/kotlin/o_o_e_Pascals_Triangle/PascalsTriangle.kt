a8260b3a22fb7cf2b8fe97008291186ae42d54c5
Misterxof <mishail.lukushevich@gmail.com>
2023-02-02 20:45:00 +0300

    118. Pascal's Triangle
    Pascal's Triangle

---
+++ src/main/kotlin/o_o_e_Pascals_Triangle/PascalsTriangle.kt
@@ -0,0 +1,27 @@
+package o_o_e_Pascals_Triangle
+
+class PascalsTriangle {
+    fun generate(numRows: Int): List<List<Int>> {
+        val result: MutableList<MutableList<Int>> = MutableList(numRows){ mutableListOf<Int>() }
+        var column = 0
+       // result[0].add(0, 1)
+        result.forEachIndexed { i, it ->
+            column++
+            for (j in 0 until column)  {
+                if (j == 0 || j == column - 1) {
+                    result[i].add(j, 1)
+                    continue
+                }
+
+                result[i].add(j, result[i-1][j-1] + result[i-1][j])
+            }
+            println(result[i])
+        }
+        println(result)
+        return result
+    }
+}
+
+fun main(args: Array<String>)  {
+    PascalsTriangle().generate(5)
+}
