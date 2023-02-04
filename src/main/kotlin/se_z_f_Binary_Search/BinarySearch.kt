c643c161bdca0494ece3b6384531862bb7c82f6c
Misterxof <mishail.lukushevich@gmail.com>
2023-02-05 01:19:40 +0300

    704. Binary Search
    Binary Search

---
+++ src/main/kotlin/se_z_f_Binary_Search/BinarySearch.kt
@@ -0,0 +1,32 @@
+package se_z_f_Binary_Search
+
+class BinarySearch {
+    fun search(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+        var prevMiddle = -1
+
+        while (true){
+            var middleIndex = (start + end) / 2
+            val middle = nums[middleIndex]
+
+            if (middle == target) return middleIndex
+
+            if (prevMiddle == middleIndex) return -1
+
+            prevMiddle = middleIndex
+
+            if (target > middle)
+                start = middleIndex + 1
+            else
+                end = middleIndex - 1
+        }
+    }
+}
+
+fun main() {
+    val binarySearch = BinarySearch()
+//    println(binarySearch.search(nums = intArrayOf(-1,0,3,5,9,12), target = 9))
+//    println(binarySearch.search(nums = intArrayOf(-1,0,3,5,9,12), target = 2))
+    println(binarySearch.search(nums = intArrayOf(2,5), target = 5))
+}
