bfca7a3f8360661b3195f1534d0f593b704f7ccd
Misterxof <mishail.lukushevich@gmail.com>
2023-02-05 16:47:55 +0300

    141. Linked List Cycle
    Linked List Cycle
    - Runtime 219 ms - 45.21%
    - Memory 36.7 MB - 77.69%

---
+++ src/main/kotlin/o_f_o_Linked_List_Cycle/LinkedListCycle.kt
@@ -0,0 +1,21 @@
+package o_f_o_Linked_List_Cycle
+
+class LinkedListCycle {
+
+    // LeetCode solution
+//    fun hasCycle(head: ListNode?): Boolean {
+//        var node = head
+//        var hashMap = hashMapOf<ListNode, Int>()
+//
+//        while (node != null) {
+//            if (hashMap.contains(node))
+//                return true
+//            else
+//                hashMap.put(node!!, 0)
+//
+//            node = node.next
+//        }
+//
+//        return false
+//    }
+}
