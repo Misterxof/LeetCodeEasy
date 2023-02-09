4d69735817160af1f4e4d5434b95d4bd78131674
Mikhail <mishail.lukushevich@gmail.com>
2023-02-09 22:17:36 +0300

    104. Maximum Depth of Binary Tree
    Maximum Depth of Binary Tree
    - Runtime 170 ms - 95.35%
    - Memory 35.4 MB - 91.12%

---
+++ src/main/kotlin/o_z_f_Maximum_Depth_Binary_Tree/MaximumDepthBinaryTree.kt
@@ -0,0 +1,17 @@
+package o_z_f_Maximum_Depth_Binary_Tree
+
+import o_f_f_Binary_Tree_Preorder_Traversal.BinaryTreePreorderTraversal
+
+class MaximumDepthBinaryTree {
+    fun maxDepth(root: BinaryTreePreorderTraversal.TreeNode?): Int {
+        var maxDepthLeft = 0
+        var maxDepthRight = 0
+
+        root?.let{
+            maxDepthLeft = maxOf(maxDepthLeft, maxDepth(it.left) + 1)
+            maxDepthRight = maxOf(maxDepthLeft, maxDepth(it.right) + 1)
+        }
+        
+        return maxOf(maxDepthLeft, maxDepthRight)
+    }
+}
