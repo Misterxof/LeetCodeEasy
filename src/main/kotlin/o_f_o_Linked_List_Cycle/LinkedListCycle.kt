af37863268df2197dc27d381d8637aa866e9eb27
Mikhail <mishail.lukushevich@gmail.com>
2023-05-16 19:12:45 +0300

    Linked List Cycle Update
    Linked List Cycle
    - Runtime 205 ms - 43.51%
    - Memory 44.5 MB - 10.31%

--- src/main/kotlin/o_f_o_Linked_List_Cycle/LinkedListCycle.kt
+++ src/main/kotlin/o_f_o_Linked_List_Cycle/LinkedListCycle.kt
@@ -2,20 +2,39 @@ package o_f_o_Linked_List_Cycle
 
 class LinkedListCycle {
 
-    // LeetCode solution
-//    fun hasCycle(head: ListNode?): Boolean {
-//        var node = head
-//        var hashMap = hashMapOf<ListNode, Int>()
-//
-//        while (node != null) {
-//            if (hashMap.contains(node))
-//                return true
-//            else
-//                hashMap.put(node!!, 0)
-//
-//            node = node.next
-//        }
-//
-//        return false
-//    }
+    fun hasCycle2(head: ListNode?): Boolean {
+        var node = head
+        var hashMap = hashMapOf<ListNode, Int>()
+
+        while (node != null) {
+            if (hashMap.contains(node))
+                return true
+            else
+                hashMap.put(node!!, 0)
+
+            node = node.next
+        }
+
+        return false
+    }
+
+    // Two pointer
+    fun hasCycle(head: ListNode?): Boolean {
+        var slowPointer = head
+        var fastPointer = head
+
+        while (slowPointer != null && fastPointer?.next != null) {
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next?.next
+
+            if (slowPointer == fastPointer)
+                return true
+        }
+
+        return false
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
 }
