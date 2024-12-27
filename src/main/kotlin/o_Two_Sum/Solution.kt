c15512844cf880f954b38d4929d313cb5d59dcd8
Mikhail <mishail.lukushevich@gmail.com>
2024-12-27 16:20:12 +0300

    1. Two Sum (Upd)
    1. Two Sum
    - Runtime 5 ms - 53.06%
    - Memory 39.95 MB - 12.28%

--- src/main/kotlin/o_Two_Sum/Solution.kt
+++ src/main/kotlin/o_Two_Sum/Solution.kt
@@ -12,4 +12,47 @@ class Solution {
         }
         return intArrayOf(0)
     }
+
+    fun twoSum2(nums: IntArray, target: Int): IntArray {
+        val hashMap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, i -> hashMap.putIfAbsent(index, i) }
+
+        hashMap.forEach { pair ->
+            val targetValue = target - pair.value
+            if (hashMap.containsValue(targetValue)) {
+//                val index = hashMap.entries.find { entry -> entry.value == targetValue && entry.key != pair.key}!!.key
+                val index = hashMap.getKey(targetValue, pair.key)
+                if (index != null)
+                    return intArrayOf(pair.key, index)
+            }
+        }
+
+        return intArrayOf()
+    }
+    
+    // the best
+    fun twoSum3(nums: IntArray, target: Int): IntArray {
+        val hashMap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, i ->
+            val targetKey = target - i
+            if (hashMap.containsKey(targetKey))
+                return intArrayOf(hashMap[targetKey]!!, index)
+            hashMap[i] = index
+        }
+
+        return intArrayOf()
+    }
+
+    fun <K,V> HashMap<K, V>.getKey(searchValue: V, avoidKey: K): K? {
+        this.entries.forEach { entry ->
+            println("k ${entry.key} v ${entry.value} sv $searchValue ak $avoidKey")
+            if (entry.value == searchValue && entry.key != avoidKey) {
+                println(entry.key)
+                return entry.key
+            }
+        }
+        return null
+    }
 }
